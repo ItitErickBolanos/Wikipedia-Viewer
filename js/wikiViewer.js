e55afc49d8e5f319c09c25d210aea725a6fdd326
ItitErickBolanos <erick.bolanos@iest.edu.mx>
2016-05-25 23:28:14 -0500

    Started coding the app
    Just got started with this new project, some things left to do include
    showing the search results and that's about it.

---
+++ js/wikiViewer.js
@@ -0,0 +1,52 @@
+var app = angular.module("wikiViewer", ['ngMaterial']);
+
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('deep-purple')
+    .accentPalette('indigo')
+    .warnPalette('yellow');
+});
+
+app.factory("wikiAPI", ['$http', function($http){
+  var wikiObj = {};
+
+  wikiObj.getWikiEntries = function(){
+    //TODO: Add the wikipedia API URL
+    var api = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=",
+        title = $("#search").val(),
+        cb = '&callback=JSON_CALLBACK';
+
+    return $http.jsonp(api + title + cb);
+  }
+
+  return wikiObj;
+}]);
+
+app.controller("wikiController", ['$scope', 'wikiAPI', '$mdDialog', function($scope, wikiAPI, $mdDialog){
+
+  $scope.goToRandom = function(){
+    window.open("https://en.wikipedia.org/wiki/Special:Random");
+  }
+
+  $scope.searchEntries = function(){
+    if ($("#search").val() != "") {
+      wikiAPI.getWikiEntries()
+      .success(function(resp){
+        
+        console.log(resp);
+      })
+      .error(function(error){
+        console.log(error);
+      });
+    } else {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('.container')))
+          .clickOutsideToClose(false)
+          .title('Warning')
+          .textContent('Please fill the input to search for an article.')
+          .ok('Ok')
+      );
+    }
+  };
+}]);
